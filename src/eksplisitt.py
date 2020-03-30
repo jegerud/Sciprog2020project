d02807fb1fe7fab70541297e14cf5f081dd20ae1
flanderzz <andekje@stud.ntnu.no>
2020-03-30 15:49:18 +0200

    flytta til egen modul++

---
+++ src/eksplisitt.py
@@ -0,0 +1,32 @@
+import numpy as np
+import unittest
+
+def eksplisitt(u, alpha=0.25, h=0, n=1000):
+    """
+    Løser diffusjonslikningen
+
+    Løser du/dt= d**2u/dx**2 +d**2u/dy**2 med n tider
+
+    Paramters
+    ---------
+    u : func
+        Funksjonen som beskriver diffusjonslikningen
+    n : int
+        Antall tidspunkter
+    h : funksjon
+        Spesifiseres ut fra problemet
+    alpha : float
+        dt/dx**2
+
+    Returns
+    -------
+    numpy.ndarray:
+        Beregnede verdier for u
+    """
+    for i in range(n):
+        u[1:-1, 1:-1]+=alpha*(u[:-2, 1:-1]+
+                          u[2:, 1:-1]+
+                          u[1:-1, :-2]+
+                          u[1:-1, 2:]-
+                          4*u[1:-1, 1:-1])
+    return u
