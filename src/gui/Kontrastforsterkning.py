7a924cea86c1c4e55c9aaf1e02d06c3fa4ae8cc9
jegerud98 <kristjeg@stud.ntnu.no>
2020-04-21 18:27:48 +0200

    Funksjonaliteter ferdig

--- src/gui/Kontrastforsterkning.py
+++ src/gui/Kontrastforsterkning.py
@@ -11,6 +11,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import Grayscale as gray
 from GlattingModul import eksplisittGlatting
+from KontrastforsterkningModul import eksplisittKontrast
 from Function import *
 from PIL import Image
 
@@ -89,8 +90,7 @@ class ContrastEnhancement(QMainWindow):
     def contrastImage(self):
         orig_im = imageio.imread(self.path).astype(float)/255 
         im = np.copy(orig_im)
-        im = im + .05 * np.random.randn(* np.shape(im))
-        im = eksplisittGlatting(im, orig_im, self.constant.value())
+        im = eksplisittKontrast(im, orig_im, self.constant.value())
 
         rescaled = (255.0 / im.max() * (im - im.min())).astype(np.uint8)
         img = Image.fromarray(rescaled)
@@ -101,8 +101,7 @@ class ContrastEnhancement(QMainWindow):
     def contrastGrayImage(self):
         orig_im =  gray.rgb2gray(self.path)
         im =  gray.rgb2gray(self.path)
-        im = im + .05 * np.random.randn(* np.shape(im))
-        im = eksplisittGlatting(im, orig_im, self.constant.value())
+        im = eksplisittKontrast(im, orig_im, self.constant.value())
 
         rescaled = (255.0 / im.max() * (im - im.min())).astype(np.uint8)
         img = Image.fromarray(rescaled)
