21010310148bed7b6983bbdcd22e9bb017f1b33a
jegerud98 <kristjeg@stud.ntnu.no>
2020-04-22 10:06:34 +0200

    Flyttet eksplitt fra anonymisering hit

--- src/eksplisitt.py
+++ src/eksplisitt.py
@@ -34,3 +34,32 @@ def eksplisitt(u, alpha=0.25, h=0, n=1000):
         u[0, :] = u[1, :]      #
         u[-1, :] = u[-2 , :]   #
     return u
+
+
+def eksplisittAnonym(u,u_0, alpha, n):
+    """
+    Løser diffusjonslikningen
+
+    Løser du/dt= d**2u/dx**2 +d**2u/dy**2 med n tider
+
+    Paramters
+    ---------
+    u : func
+        Funksjonen som beskriver diffusjonslikningen
+    u_0: func
+        Original u
+    alpha : float
+        dt/dx**2
+    n : int
+        Antall tidspunkter
+
+    Returns
+    -------
+    numpy.ndarray:
+        Beregnede verdier for u
+    """
+    for i in range(n):
+        u[1:-1, 1:-1] += alpha * (u[:-2, 1:-1] + u[2:, 1:-1] + u[1:-1, :-2] + u[1:-1, 2:]-4 * u[1:-1, 1:-1]) 
+        u[0] = u_0[0]   #Dirichlet randbetingelser
+        u[-1] = u_0[-1] #
+    return u
