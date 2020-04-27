5a402eda8fbd77c60e3279c268c9ef8cefdc766d
jegerud98 <kristjeg@stud.ntnu.no>
2020-04-27 19:30:18 +0200

    Fikset på inpaintingmetoden

--- src/InpaintingModul.py
+++ src/InpaintingModul.py
@@ -5,50 +5,30 @@ import Eksplisitt as eks
 import Grayscale as gray
 import ImageView as imv
 
-def Inpaint(file, ret = 1):
+
+def Inpaint(file, ret=1, alpha=.25, n=100, colour=True):
     im = imageio.imread(file)
-    im = np.sum(im.astype(float), 2) / (3 * 255)  #gråtone
-    im0 = np.copy(im)
+    orig_im = np.copy(im)
+    if colour:
+        im = im.astype(dtype=float) / 255
+        gray_im =np.sum(im.astype(float), 2) / (3 * 255)
+        mask = np.zeros(gray_im.shape)
+    else:
+        im = np.sum(im.astype(float), 2) / (3 * 255)  #gråtone
+        mask = np.zeros(im.shape)     #lag maske
     im[im < 0] = 0                                 # klipp til lovlige verdier
     im[im > 1] = 1
 
-    mask = np.ones(im.shape)     #lag maske
-    mask[350:400 ,450:550] = 0
-    mask[500:650 ,300:500] = 0
-    mask[200:300 ,700:800] = 0
+    mask[135:140, 250:380] = 1
+    mask[250:470, 300:305] = 1
+    mask[740:755, 550:700] = 1
     mask = mask.astype(bool)
-    
-    im=eks.eksplisitt(im, n=20)  #løs
-    im[mask] = im0[mask]         #ja
-    
+
+    im[mask] = 0
+    im, im0 = eks.eksplisittInpaint(im, mask)
+
     if ret == 1:
-        return im0, mask, im
-    elif ret == 2:
-        return mask
-    else:
-        return im
-
-
-def Inpaint_rgb(file, ret = 1):
-    orig_im = imageio.imread(file)
-    im=np.copy(orig_im)
-    im = im.astype(dtype=float) / 255
-    gray_im =np.sum(im.astype(float), 2) / (3 * 255)  #gråtone trengs for å vise masken
-    im0 = np.copy(im)
-    im[im < 0] = 0                # klipp til lovlige verdier
-    im[im > 1] = 1
-
-    mask = np.ones(gray_im.shape) #lag maske
-    mask[350:400 ,450:550] = 0
-    mask[500:650 ,300:500] = 0
-    mask[200:300 ,700:800] = 0
-    mask = mask.astype(bool)      #lag bool-array
-
-    im=eks.eksplisitt(im, n=20)   #løs
-
-    im[mask] = im0[mask]          #ja
-    if ret == 1:
-        return orig_im, mask, im
+        return orig_im, im0, im
     elif ret == 2:
         return mask
     else:
