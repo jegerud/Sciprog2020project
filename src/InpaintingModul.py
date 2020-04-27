24bfc31272f02e16db44589f48da18259015b5e9
jegerud98 <kristjeg@stud.ntnu.no>
2020-04-27 12:07:59 +0200

    Småfiks på gråtone versjonen

--- src/InpaintingModul.py
+++ src/InpaintingModul.py
@@ -2,11 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import imageio
 import Eksplisitt as eks
+import Grayscale as gray
 import ImageView as imv
 
 def Inpaint(file, ret = 1):
-    orig_im = imageio.imread(file)
-    im=np.copy(orig_im)
+    im = imageio.imread(file)
     im = np.sum(im.astype(float), 2) / (3 * 255)  #gråtone
     im0 = np.copy(im)
     im[im < 0] = 0                                 # klipp til lovlige verdier
@@ -17,11 +17,12 @@ def Inpaint(file, ret = 1):
     mask[500:650 ,300:500] = 0
     mask[200:300 ,700:800] = 0
     mask = mask.astype(bool)
+    
     im=eks.eksplisitt(im, n=20)  #løs
-
     im[mask] = im0[mask]         #ja
+    
     if ret == 1:
-        return orig_im, mask, im
+        return im0, mask, im
     elif ret == 2:
         return mask
     else:
