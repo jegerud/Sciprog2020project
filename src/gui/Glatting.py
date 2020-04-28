02e5f3b2aa71c038d68a27cdf05d1d36bd4873b5
jegerud98 <kristjeg@stud.ntnu.no>
2020-04-28 11:33:10 +0200

    Refactoring

--- src/gui/Glatting.py
+++ src/gui/Glatting.py
@@ -13,6 +13,7 @@ import Grayscale as gray
 from GlattingModul import eksplisittGlatting
 from Function import *
 from PIL import Image
+from functools import partial
 
 class Blur(QMainWindow):
     def __init__(self):
@@ -21,51 +22,32 @@ class Blur(QMainWindow):
         self.path = "../../hdr-bilder/Balls/Balls_00032.png"
         self.blurImg.setPixmap(QtGui.QPixmap(self.path))
 
-        self.adjuster.clicked.connect(self.showAdjuster)
-        self.balls.clicked.connect(self.showBalls)
-        self.fog.clicked.connect(self.showFog)
-        self.garden.clicked.connect(self.showGarden)
-        self.mountains.clicked.connect(self.showMountains)
-        self.ocean.clicked.connect(self.showOcean)
-        self.stillife.clicked.connect(self.showStillife)
-        self.trees.clicked.connect(self.showTrees)
+        self.imgOne = "../../hdr-bilder/Adjuster/Adjuster_00032.png"
+        self.imgTwo = "../../hdr-bilder/Balls/Balls_00032.png"
+        self.imgThree = "../../hdr-bilder/Fog/Fog_00128.png"
+        self.imgFour = "../../hdr-bilder/Garden/Garden_00004.png"
+        self.imgFive = "../../hdr-bilder/MtTamNorth/MtTamNorth_00004.png"
+        self.imgSix = "../../hdr-bilder/Ocean/Ocean_00256.png"
+        self.imgSeven = "../../hdr-bilder/StillLife/StillLife_01024.png"
+        self.imgEight = "../../hdr-bilder/Tree/Tree_00064.png"
+
+        self.adjuster.clicked.connect(partial(self.setImage, self.imgOne))
+        self.balls.clicked.connect(partial(self.setImage, self.imgTwo))
+        self.fog.clicked.connect(partial(self.setImage, self.imgThree))
+        self.garden.clicked.connect(partial(self.setImage, self.imgFour))
+        self.mountains.clicked.connect(partial(self.setImage, self.imgFive))
+        self.ocean.clicked.connect(partial(self.setImage, self.imgSix))
+        self.stillife.clicked.connect(partial(self.setImage, self.imgSeven))
+        self.trees.clicked.connect(partial(self.setImage, self.imgEight))
         self.blurCode.clicked.connect(self.showCode)
         self.blurGray.clicked.connect(self.blurGrayImage)
         self.blurOriginal.clicked.connect(self.setOriginal)
         self.blurColour.clicked.connect(self.blurImage)
         self.blurOrigGray.clicked.connect(self.setGray)
 
-    def showAdjuster(self):
-        self.path = "../../hdr-bilder/Adjuster/Adjuster_00032.png"
-        self.blurImg.setPixmap(QtGui.QPixmap(self.path))
-
-    def showBalls(self):
-        self.path = "../../hdr-bilder/Balls/Balls_00032.png"
-        self.blurImg.setPixmap(QtGui.QPixmap(self.path))
-
-    def showFog(self):
-        self.path = "../../hdr-bilder/Fog/Fog_00128.png"
-        self.blurImg.setPixmap(QtGui.QPixmap(self.path))
-
-    def showGarden(self):
-        self.path = "../../hdr-bilder/Garden/Garden_00004.png"
-        self.blurImg.setPixmap(QtGui.QPixmap(self.path))
-
-    def showMountains(self):
-        self.path = "../../hdr-bilder/MtTamNorth/MtTamNorth_00004.png"
-        self.blurImg.setPixmap(QtGui.QPixmap(self.path))
-
-    def showOcean(self):
-        self.path = "../../hdr-bilder/Ocean/Ocean_00256.png"
-        self.blurImg.setPixmap(QtGui.QPixmap(self.path))
-    
-    def showStillife(self):
-        self.path = "../../hdr-bilder/StillLife/StillLife_01024.png"
-        self.blurImg.setPixmap(QtGui.QPixmap(self.path))
-
-    def showTrees(self):
-        self.path = "../../hdr-bilder/Tree/Tree_00064.png"
-        self.blurImg.setPixmap(QtGui.QPixmap(self.path))
+    def setImage(self, img):
+        self.path = img
+        self.blurImg.setPixmap(QtGui.QPixmap(img))
     
     def showCode(self):
         code = QPlainTextEdit()
