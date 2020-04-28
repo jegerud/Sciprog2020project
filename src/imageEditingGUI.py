506a61bc5de70f9ab8b9c52f01f28361726296fb
jegerud98 <kristjeg@stud.ntnu.no>
2020-04-28 13:58:00 +0200

    Småfiks

--- src/imageEditingGUI.py
+++ src/imageEditingGUI.py
@@ -2,7 +2,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets, uic
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
-from gui import AnonymiseGUI, BlurGUI, ContrastEnhancementGUI, ConvertGrayscaleGUI, DemosaicingGUI, InpaintingGUI, SeamlessGUI
+from gui import *
 import sys
 
 class Home(QMainWindow):
