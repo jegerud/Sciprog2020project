bbcc8538608b56bbe79a0508c5842da48e72a91c
jegerud98 <kristjeg@stud.ntnu.no>
2020-04-28 11:34:28 +0200

    Refactoring

--- src/gui/Kontrastforsterkning.py
+++ src/gui/Kontrastforsterkning.py
@@ -13,6 +13,7 @@ import Grayscale as gray
 from Eksplisitt import eksplisittKontrast
 from Function import *
 from PIL import Image
+from functools import partial
 
 class ContrastEnhancement(QMainWindow):
     def __init__(self):
@@ -21,52 +22,33 @@ class ContrastEnhancement(QMainWindow):
         self.path = "../../hdr-bilder/Balls/Balls_00032.png"
         self.contrastImg.setPixmap(QtGui.QPixmap(self.path))
 
-        self.adjuster.clicked.connect(self.showAdjuster)
-        self.balls.clicked.connect(self.showBalls)
-        self.fog.clicked.connect(self.showFog)
-        self.garden.clicked.connect(self.showGarden)
-        self.mountains.clicked.connect(self.showMountains)
-        self.ocean.clicked.connect(self.showOcean)
-        self.stillife.clicked.connect(self.showStillife)
-        self.trees.clicked.connect(self.showTrees)
+        self.imgOne = "../../hdr-bilder/Adjuster/Adjuster_00032.png"
+        self.imgTwo = "../../hdr-bilder/Balls/Balls_00032.png"
+        self.imgThree = "../../hdr-bilder/Fog/Fog_00128.png"
+        self.imgFour = "../../hdr-bilder/Garden/Garden_00004.png"
+        self.imgFive = "../../hdr-bilder/MtTamNorth/MtTamNorth_00004.png"
+        self.imgSix = "../../hdr-bilder/Ocean/Ocean_00256.png"
+        self.imgSeven = "../../hdr-bilder/StillLife/StillLife_01024.png"
+        self.imgEight = "../../hdr-bilder/Tree/Tree_00064.png"
+
+        self.adjuster.clicked.connect(partial(self.setImage, self.imgOne))
+        self.balls.clicked.connect(partial(self.setImage, self.imgTwo))
+        self.fog.clicked.connect(partial(self.setImage, self.imgThree))
+        self.garden.clicked.connect(partial(self.setImage, self.imgFour))
+        self.mountains.clicked.connect(partial(self.setImage, self.imgFive))
+        self.ocean.clicked.connect(partial(self.setImage, self.imgSix))
+        self.stillife.clicked.connect(partial(self.setImage, self.imgSeven))
+        self.trees.clicked.connect(partial(self.setImage, self.imgEight))
         self.contrastCode.clicked.connect(self.showCode)
         self.contrastGray.clicked.connect(self.contrastGrayImage)
         self.contrastOriginal.clicked.connect(self.setOriginal)
         self.contrastColour.clicked.connect(self.contrastImage)
         self.contrastOrigGray.clicked.connect(self.setGray)
 
-    def showAdjuster(self):
-        self.path = "../../hdr-bilder/Adjuster/Adjuster_00032.png"
-        self.contrastImg.setPixmap(QtGui.QPixmap(self.path))
+    def setImage(self, img):
+        self.path = img
+        self.demosaicImg.setPixmap(QtGui.QPixmap(img))
 
-    def showBalls(self):
-        self.path = "../../hdr-bilder/Balls/Balls_00032.png"
-        self.contrastImg.setPixmap(QtGui.QPixmap(self.path))
-
-    def showFog(self):
-        self.path = "../../hdr-bilder/Fog/Fog_00128.png"
-        self.contrastImg.setPixmap(QtGui.QPixmap(self.path))
-
-    def showGarden(self):
-        self.path = "../../hdr-bilder/Garden/Garden_00004.png"
-        self.contrastImg.setPixmap(QtGui.QPixmap(self.path))
-
-    def showMountains(self):
-        self.path = "../../hdr-bilder/MtTamNorth/MtTamNorth_00004.png"
-        self.contrastImg.setPixmap(QtGui.QPixmap(self.path))
-
-    def showOcean(self):
-        self.path = "../../hdr-bilder/Ocean/Ocean_00256.png"
-        self.contrastImg.setPixmap(QtGui.QPixmap(self.path))
-    
-    def showStillife(self):
-        self.path = "../../hdr-bilder/StillLife/StillLife_01024.png"
-        self.contrastImg.setPixmap(QtGui.QPixmap(self.path))
-
-    def showTrees(self):
-        self.path = "../../hdr-bilder/Tree/Tree_00064.png"
-        self.contrastImg.setPixmap(QtGui.QPixmap(self.path))
-    
     def showCode(self):
         code = QPlainTextEdit()
         text = open('codes/kontrastforsterkning.txt').read()
