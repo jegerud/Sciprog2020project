85e8c1d62719983d1f3b0e075fdb78b307f65623
jegerud98 <kristjeg@stud.ntnu.no>
2020-04-28 11:29:54 +0200

    refactoring

--- src/gui/Demosaicing.py
+++ src/gui/Demosaicing.py
@@ -12,6 +12,7 @@ import matplotlib.pyplot as plt
 from DemosaicingModul import *
 from Function import *
 from PIL import Image
+from functools import partial
 
 class Demosaic(QMainWindow):
     def __init__(self):
@@ -20,14 +21,23 @@ class Demosaic(QMainWindow):
         self.path = "../../hdr-bilder/Balls/Balls_00032.png"
         self.demosaicImg.setPixmap(QtGui.QPixmap(self.path))
 
-        self.adjuster.clicked.connect(self.showAdjuster)
-        self.balls.clicked.connect(self.showBalls)
-        self.fog.clicked.connect(self.showFog)
-        self.garden.clicked.connect(self.showGarden)
-        self.mountains.clicked.connect(self.showMountains)
-        self.ocean.clicked.connect(self.showOcean)
-        self.stillife.clicked.connect(self.showStillife)
-        self.trees.clicked.connect(self.showTrees)
+        self.imgOne = "../../hdr-bilder/Adjuster/Adjuster_00032.png"
+        self.imgTwo = "../../hdr-bilder/Balls/Balls_00032.png"
+        self.imgThree = "../../hdr-bilder/Fog/Fog_00128.png"
+        self.imgFour = "../../hdr-bilder/Garden/Garden_00004.png"
+        self.imgFive = "../../hdr-bilder/MtTamNorth/MtTamNorth_00004.png"
+        self.imgSix = "../../hdr-bilder/Ocean/Ocean_00256.png"
+        self.imgSeven = "../../hdr-bilder/StillLife/StillLife_01024.png"
+        self.imgEight = "../../hdr-bilder/Tree/Tree_00064.png"
+
+        self.adjuster.clicked.connect(partial(self.setImage, self.imgOne))
+        self.balls.clicked.connect(partial(self.setImage, self.imgTwo))
+        self.fog.clicked.connect(partial(self.setImage, self.imgThree))
+        self.garden.clicked.connect(partial(self.setImage, self.imgFour))
+        self.mountains.clicked.connect(partial(self.setImage, self.imgFive))
+        self.ocean.clicked.connect(partial(self.setImage, self.imgSix))
+        self.stillife.clicked.connect(partial(self.setImage, self.imgSeven))
+        self.trees.clicked.connect(partial(self.setImage, self.imgEight))
         self.demosaicCode.clicked.connect(self.showCode)
         self.demosaicOriginal.clicked.connect(self.setOriginal)
         self.demosaicOriginal2.clicked.connect(self.setOriginal)
@@ -36,37 +46,9 @@ class Demosaic(QMainWindow):
         self.demosaicDemosaic.clicked.connect(self.demosaicImage)
         self.demosaicDemosaicPackage.clicked.connect(self.demosaicImagePackage)
 
-    def showAdjuster(self):
-        self.path = "../../hdr-bilder/Adjuster/Adjuster_00032.png"
-        self.demosaicImg.setPixmap(QtGui.QPixmap(self.path))
-
-    def showBalls(self):
-        self.path = "../../hdr-bilder/Balls/Balls_00032.png"
-        self.demosaicImg.setPixmap(QtGui.QPixmap(self.path))
-
-    def showFog(self):
-        self.path = "../../hdr-bilder/Fog/Fog_00128.png"
-        self.demosaicImg.setPixmap(QtGui.QPixmap(self.path))
-
-    def showGarden(self):
-        self.path = "../../hdr-bilder/Garden/Garden_00004.png"
-        self.demosaicImg.setPixmap(QtGui.QPixmap(self.path))
-
-    def showMountains(self):
-        self.path = "../../hdr-bilder/MtTamNorth/MtTamNorth_00004.png"
-        self.demosaicImg.setPixmap(QtGui.QPixmap(self.path))
-
-    def showOcean(self):
-        self.path = "../../hdr-bilder/Ocean/Ocean_00256.png"
-        self.demosaicImg.setPixmap(QtGui.QPixmap(self.path))
-    
-    def showStillife(self):
-        self.path = "../../hdr-bilder/StillLife/StillLife_01024.png"
-        self.demosaicImg.setPixmap(QtGui.QPixmap(self.path))
-
-    def showTrees(self):
-        self.path = "../../hdr-bilder/Tree/Tree_00064.png"
-        self.demosaicImg.setPixmap(QtGui.QPixmap(self.path))
+    def setImage(self, img):
+        self.path = img
+        self.demosaicImg.setPixmap(QtGui.QPixmap(img))
     
     def showCode(self):
         code = QPlainTextEdit()
