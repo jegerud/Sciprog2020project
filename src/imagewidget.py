f29771f7172b88d22a948a68fd75a398648df9cd
jegerud98 <kristjeg@stud.ntnu.no>
2020-05-06 14:38:38 +0200

    Refactoring

--- src/imagewidget.py
+++ src/imagewidget.py
@@ -16,17 +16,13 @@ class imagewidget(FigureCanvas):
         FigureCanvas.__init__(self, self.img)
         self.setParent(parent)
 
-    def showGrayImage(self, image):
+    def showImage(self, image, colour=True):
         self.img = self.figure.add_subplot(111)
         self.img.axis("off")
-        self.img.imshow(image, plt.cm.gray)
-        self.figure.subplots_adjust(left=0.001, right=0.999, top=0.999, bottom=0.001)
-        self.draw()
-
-    def showImage(self, image):
-        self.img = self.figure.add_subplot(111)
-        self.img.axis("off")
-        self.img.imshow(image)
+        if colour:
+            self.img.imshow(image)
+        else:
+            self.img.imshow(image, plt.cm.gray)
         self.figure.subplots_adjust(left=0.001, right=0.999, top=0.999, bottom=0.001)
         self.draw()
         
