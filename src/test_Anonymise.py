4990d7f67069e96cfde62fdd71da560c4fe80ac0
casperfg <casperfg@stud.ntnu.no>
2020-05-12 21:51:09 +0200

    Test endringer

--- src/test_Anonymise.py
+++ src/test_Anonymise.py
@@ -3,7 +3,8 @@ from Source.Anonymise import blurFace
 from Source.Anonymise import detectFace
 
 lena = '../hdr-bilder/Faces/lena.png'
-team = '../hdr-bilder/Faces/team.jpg'
+group1 = '../hdr-bilder/Faces/group1.jpg'
+couple = '../hdr-bilder/Faces/couple.jpg'
 
 class test_Anon(unittest.TestCase):
     """
@@ -11,17 +12,25 @@ class test_Anon(unittest.TestCase):
     """
     def test_faceDetect_Lena(self):
         """
-        Test for face detection of image (lena)
+        Test for face detection of image (lena) 
         """
         lena_antall, lena_image = detectFace(lena)
         lena_expectedNumber = 1
         self.assertEqual(lena_antall, lena_expectedNumber)
      
     
-    def test_faceDetect_Team(self):
+    def test_faceDetect_Group1(self):
         """
-        Test for face detection of image (team)
+        Test for face detection of image (group1.jpg)
         """
-        team_antall, team_image = detectFace(team, 1.25037, 3)
-        team_expectedNumber = 8
-        self.assertEqual(team_antall, team_expectedNumber)
+        group1_antall, group1_image = detectFace(group1)
+        group1_expectedNumber = 7
+        self.assertEqual(group1_antall, group1_expectedNumber)
+        
+    def test_faceDetect_Couple(self):
+        """
+        Test for face detection of image (couple.jpg)
+        """
+        couple_antall, couple_image = detectFace(couple)
+        couple_expectedNumber = 2
+        self.assertEqual(couple_antall, couple_expectedNumber)
