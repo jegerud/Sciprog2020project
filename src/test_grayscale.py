437547bb453017d60092bb7e5d5f66c6945bc5b9
casperfg <casperfg@stud.ntnu.no>
2020-05-14 13:35:24 +0200

    Lag til enhetstest for grayscale

---
+++ src/test_grayscale.py
@@ -0,0 +1,20 @@
+import unittest
+import imageio
+import cv2
+import Source.Grayscale as gray
+
+file = '../hdr-bilder/Balls/Balls_00016.png'
+
+class test_grayscale(unittest.TestCase):
+    def test_isgray(self):
+        grayImage = gray.rgb2gray(file)
+        shape = (len(grayImage.shape))
+       
+        self.assertLess(shape,3)
+        
+    def test_is_not_gray(self):
+        image = imageio.imread(file)
+        shape = (len(image.shape))
+        
+        self.assertEqual(shape,3)
+        
